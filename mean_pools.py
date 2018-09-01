582ee880d5c9a5d7edeb04b658b3a78c42bac3ca
Chitralekha Nagalingam <chitrashankar673@gmail.com>
2018-09-01 11:12:27 +0530

    more changes to README

--- mean_pools.py
+++ mean_pools.py
@@ -4,16 +4,20 @@ import cv2
 import numpy as np
 
 if __name__=="__main__":
-    img_path = 'C:/Users/chitrale/Desktop/samp.jpg'
+    if(len(sys.argv)<2):
+        print ("Usage :: python max_pool.py <img_path> 3")
+        sys.exit()    
+    img_path = sys.argv[1]
+    pool_size = int(sys.argv[2])
+    
     img = cv2.imread(img_path,0)
     rows = img.shape[0]
     cols = img.shape[1]
-    #pool_size = int(input("Enter mean_pool:"))
-    pool_size = 3
+    
     pool_arr=[]
-    for r in range(0,rows-pool_size+1):
+    for r in range(rows-pool_size+1):
         pool_col=[]
-        for c in range(0,cols-pool_size+1):
+        for c in range(cols-pool_size+1):
             m = np.mean(img[r:r+pool_size,c:c+pool_size])
             pool_col.append(m) 
         pool_arr.append(pool_col)    
